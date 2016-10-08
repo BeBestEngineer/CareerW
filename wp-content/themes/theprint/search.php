f0230d69a595165fb1ef3f19fc7e39599e5e7231
Alexandr Gorchakov <385898sw@gmail.com>
2016-10-08 14:55:35 +0600

    disable breadcrumbs from search

--- wp-content/themes/theprint/search.php
+++ wp-content/themes/theprint/search.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	<div class="gp-theprint-row">
 		<div class="gp-theprint-site-main">
 			<main id="content" class="gp-theprint-content">
-				<?php echo gp_theprint_breadcrumbs(); ?>
+				<?php //echo gp_theprint_breadcrumbs(); ?>
 
 				<?php if (!have_posts()) : ?>
 					<?php get_template_part('template-parts/content', 'none'); ?>
