b468211f6a9501a09f4a9bb39ef2c22e3c5ab58a
Alexandr Gorchakov <385898sw@gmail.com>
2016-10-08 14:44:30 +0600

    translate Search...

--- wp-content/themes/theprint/searchform.php
+++ wp-content/themes/theprint/searchform.php
@@ -10,7 +10,7 @@
 
 <form class="gp-theprint-search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<input type="hidden" id="post_type" name="post_type" value="post" />
-	<input type="text" id="search" name="s" placeholder="<?php esc_attr_e('Search...', 'theprint'); ?>"/>
+	<input type="text" id="search" name="s" placeholder="<?php esc_attr_e('Поиск...', 'theprint'); ?>"/>
 
 	<button type="submit" id="button"><i class="fa fa-search"></i></button>
 </form><!-- .gp-search-form -->
